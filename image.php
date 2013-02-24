de63899524c3cc763147ab95e77db5e5e0072d3d
Gareth Cooper <gareth@garethcooper.com>
2013-02-24 23:08:54 +0000

    Initial work on image attachments page

--- image.php
+++ image.php
@@ -8,48 +8,47 @@
 
 get_header(); ?>
 
-		<div id="primary" class="image-attachment">
-			<div id="content" role="main">
+<div id="content" class="yui3-g central">
+	<div class="yui3-u-1">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+		<?php while ( have_posts() ) : the_post(); ?>
 
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<header class="entry-header">
-						<h1 class="entry-title"><?php the_title(); ?></h1>
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<header class="entry-header">
+				<h1 class="entry-title">
+					<?php the_title(); ?>
+				</h1>
 
-						<div class="entry-meta">
-							<?php
-								$metadata = wp_get_attachment_metadata();
-								printf( __( 'Published <span class="entry-date"><abbr class="published" title="%1$s">%2$s</abbr></span> at <a href="%3$s" title="Link to full-size image">%4$s &times; %5$s</a> in <a href="%6$s" title="Return to %7$s" rel="gallery">%7$s</a>', 'garethcooper' ),
-									esc_attr( get_the_time() ),
-									get_the_date(),
-									wp_get_attachment_url(),
-									$metadata['width'],
-									$metadata['height'],
-									get_permalink( $post->post_parent ),
-									get_the_title( $post->post_parent )
-								);
-							?>
-							<?php edit_post_link( __( 'Edit', 'garethcooper' ), '<span class="sep">|</span> <span class="edit-link">', '</span>' ); ?>
-						</div><!-- .entry-meta -->
+				<div class="entry-meta">
+					<?php
+					$metadata = wp_get_attachment_metadata();
 
-						<nav id="image-navigation">
-							<span class="previous-image"><?php previous_image_link( false, __( '&larr; Previous' , 'garethcooper' ) ); ?></span>
-							<span class="next-image"><?php next_image_link( false, __( 'Next &rarr;' , 'garethcooper' ) ); ?></span>
-						</nav><!-- #image-navigation -->
-					</header><!-- .entry-header -->
+					printf( __( 'Published <span class="entry-date"><abbr class="published" title="%1$s">%2$s</abbr></span> at <a href="%3$s" title="Link to full-size image">%4$s &times; %5$s</a> in <a href="%6$s" title="Return to %7$s" rel="gallery">%7$s</a>', 'garethcooper' ),
+					esc_attr( get_the_time() ),
+					get_the_date(),
+					wp_get_attachment_url(),
+					$metadata['width'],
+					$metadata['height'],
+					get_permalink( $post->post_parent ),
+					get_the_title( $post->post_parent )
+					);
+					?>
+				</div>
+				<!-- .entry-meta -->
+			</header>
+			<!-- .entry-header -->
 
-					<div class="entry-content">
+			<div class="entry-content">
 
-						<div class="entry-attachment">
-							<div class="attachment">
-								<?php
-									/**
-									 * Grab the IDs of all the image attachments in a gallery so we can get the URL of the next adjacent image in a gallery,
-									 * or the first image (if we're looking at the last image in a gallery), or, in a gallery of one, just the link to that image file
-									 */
-									$attachments = array_values( get_children( array( 'post_parent' => $post->post_parent, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => 'ASC', 'orderby' => 'menu_order ID' ) ) );
-									foreach ( $attachments as $k => $attachment ) {
+				<div class="entry-attachment">
+					<div class="attachment">
+						<?php
+						/**
+						 * Grab the IDs of all the image attachments in a gallery so we can get the URL of the next adjacent image in a gallery,
+						 * or the first image (if we're looking at the last image in a gallery), or, in a gallery of one, just the link to that image file
+						 */
+						$attachments = array_values( get_children( array( 'post_parent' => $post->post_parent, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => 'ASC', 'orderby' => 'menu_order ID' ) ) );
+						foreach ( $attachments as $k => $attachment ) {
 										if ( $attachment->ID == $post->ID )
 											break;
 									}
@@ -66,45 +65,79 @@ get_header(); ?>
 										// or, if there's only 1 image, get the URL of the image
 										$next_attachment_url = wp_get_attachment_url();
 									}
-								?>
+									?>
 
-								<a href="<?php echo $next_attachment_url; ?>" title="<?php echo esc_attr( get_the_title() ); ?>" rel="attachment"><?php
-								$attachment_size = apply_filters( 'garethcooper_attachment_size', 1200 );
-								echo wp_get_attachment_image( $post->ID, array( $attachment_size, $attachment_size ) ); // filterable image width with, essentially, no limit for image height.
-								?></a>
-							</div><!-- .attachment -->
+						<a href="<?php echo $next_attachment_url; ?>"
+							title="<?php echo esc_attr( get_the_title() ); ?>"
+							rel="attachment"><?php
+							$attachment_size = apply_filters( 'garethcooper_attachment_size', 1200 );
+							echo wp_get_attachment_image( $post->ID, array( $attachment_size, $attachment_size ) ); // filterable image width with, essentially, no limit for image height.
+							?> </a>
+					</div>
+					<!-- .attachment -->
 
-							<?php if ( ! empty( $post->post_excerpt ) ) : ?>
-							<div class="entry-caption">
-								<?php the_excerpt(); ?>
-							</div>
-							<?php endif; ?>
-						</div><!-- .entry-attachment -->
+					<?php if ( ! empty( $post->post_excerpt ) ) : ?>
+					<div class="entry-caption">
+						<?php the_excerpt(); ?>
+					</div>
+					<?php endif; ?>
+				</div>
+				<!-- .entry-attachment -->
 
-						<?php the_content(); ?>
-						<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'garethcooper' ), 'after' => '</div>' ) ); ?>
+				<?php the_content(); ?>
+				<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'garethcooper' ), 'after' => '</div>' ) ); ?>
 
-					</div><!-- .entry-content -->
+				
+				<h2>Image Data</h2>
+				
+				<p>Published on: <time style="background:none;padding:0;" datetime="<?php echo get_the_date( 'c' ); ?>"><?php echo get_the_date(); ?></time>
+				Taken On: <?php echo $metadata['image_meta']['created_timestamp']; ?>
+				Aperture: f/<?php echo $metadata['image_meta']['aperture']; ?>
+				Shutter speed: <?php
+				if ($metadata['image_meta']['shutter_speed'] < 1) {
+					echo '1/' . 1/$metadata['image_meta']['shutter_speed'];
+				} else {
+					echo $metadata['image_meta']['shutter_speed'];
+				} ?>s
+				ISO: <?php echo $metadata['image_meta']['iso']; ?>
+				Focal Length: <?php echo $metadata['image_meta']['focal_length']; ?>mm
+				Camera: <?php echo $metadata['image_meta']['camera']; ?></p>
+				
+				<pre>
+				<?php print_r($metadata); ?>
+				</pre>
+			</div>
+			<!-- .entry-content -->
 
-					<footer class="entry-meta">
-						<?php if ( comments_open() && pings_open() ) : // Comments and trackbacks open ?>
-							<?php printf( __( '<a class="comment-link" href="#respond" title="Post a comment">Post a comment</a> or leave a trackback: <a class="trackback-link" href="%s" title="Trackback URL for your post" rel="trackback">Trackback URL</a>.', 'garethcooper' ), get_trackback_url() ); ?>
-						<?php elseif ( ! comments_open() && pings_open() ) : // Only trackbacks open ?>
-							<?php printf( __( 'Comments are closed, but you can leave a trackback: <a class="trackback-link" href="%s" title="Trackback URL for your post" rel="trackback">Trackback URL</a>.', 'garethcooper' ), get_trackback_url() ); ?>
-						<?php elseif ( comments_open() && ! pings_open() ) : // Only comments open ?>
-							<?php _e( 'Trackbacks are closed, but you can <a class="comment-link" href="#respond" title="Post a comment">post a comment</a>.', 'garethcooper' ); ?>
-						<?php elseif ( ! comments_open() && ! pings_open() ) : // Comments and trackbacks closed ?>
-							<?php _e( 'Both comments and trackbacks are currently closed.', 'garethcooper' ); ?>
-						<?php endif; ?>
-						<?php edit_post_link( __( 'Edit', 'garethcooper' ), ' <span class="edit-link">', '</span>' ); ?>
-					</footer><!-- .entry-meta -->
-				</article><!-- #post-<?php the_ID(); ?> -->
+			<nav id="image-navigation">
+				<span class="previous-image"><?php previous_image_link( false, __( '&larr; Previous' , 'garethcooper' ) ); ?>
+				</span> <span class="next-image"><?php next_image_link( false, __( 'Next &rarr;' , 'garethcooper' ) ); ?>
+				</span>
+			</nav>
+			<!-- #image-navigation -->
 
-				<?php comments_template(); ?>
+			<footer class="entry-meta">
+				<?php if ( comments_open() && pings_open() ) : // Comments and trackbacks open ?>
+				<?php printf( __( '<a class="comment-link" href="#respond" title="Post a comment">Post a comment</a> or leave a trackback: <a class="trackback-link" href="%s" title="Trackback URL for your post" rel="trackback">Trackback URL</a>.', 'garethcooper' ), get_trackback_url() ); ?>
+				<?php elseif ( ! comments_open() && pings_open() ) : // Only trackbacks open ?>
+				<?php printf( __( 'Comments are closed, but you can leave a trackback: <a class="trackback-link" href="%s" title="Trackback URL for your post" rel="trackback">Trackback URL</a>.', 'garethcooper' ), get_trackback_url() ); ?>
+				<?php elseif ( comments_open() && ! pings_open() ) : // Only comments open ?>
+				<?php _e( 'Trackbacks are closed, but you can <a class="comment-link" href="#respond" title="Post a comment">post a comment</a>.', 'garethcooper' ); ?>
+				<?php elseif ( ! comments_open() && ! pings_open() ) : // Comments and trackbacks closed ?>
+				<?php _e( 'Both comments and trackbacks are currently closed.', 'garethcooper' ); ?>
+				<?php endif; ?>
+			</footer>
+			<!-- .entry-meta -->
+		</article>
+		<!-- #post-<?php the_ID(); ?> -->
 
-			<?php endwhile; // end of the loop. ?>
+		<?php comments_template(); ?>
 
-			</div><!-- #content -->
-		</div><!-- #primary -->
+		<?php endwhile; // end of the loop. ?>
+
+	</div>
+	<!-- #content -->
+</div>
+<!-- #primary -->
 
 <?php get_footer(); ?>
